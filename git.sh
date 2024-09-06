88104833cbb6ff10d8d4097d3b4d65aade1a48a5
Akash Kumar <akashkumar.xda@gmail.com>
2024-09-06 19:42:13 +0530

    Github pages update

--- git.sh
+++ git.sh
@@ -1,3 +1,5 @@
+ng build --configuration production --base-href "https://github.com/akashkr101/portfolio"
+npx angular-cli-ghpages --dir=dist/portfolio/browser
 git add .
 echo "Enter the commit message: "
 read message
